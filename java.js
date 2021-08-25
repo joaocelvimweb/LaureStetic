9c94fe55aa556bfa85d752144aeef403a50e2a25
Joao Celvim <joaocelvim.web@hotmail.com>
2021-08-25 00:55:23 -0400

    config.mobile2

---
+++ java.js
@@ -0,0 +1,62 @@
+// console.log("joao" +" "+ "celvim")
+// concatenação de string: Espaçamento entre palavras ( + " " + obs: Espaço entre as aspas para gerar o espaço)
+
+
+console.log("5" - 1)
+    
+
+
+// variavel: valor ou quantidade variado
+
+// var qtd=5
+// console.log(qtd)
+
+
+// 1-resposta
+console.log("joao" +" "+ "celvim" +" "+ "valhovera")
+
+
+// 2-resposta 
+var n1=5
+var n2=3
+console.log(n1 + n2)
+console.log (n1 - n2)
+
+// 3-resposta
+var n1=30
+var n2=4
+console.log(n1 / n2)
+console.log(n2 % n1)
+
+
+// 4-resposta
+var n1=10
+var n2=50
+if (n1=>n2) {
+    console.log("falso")
+ } else {
+    console.log("verdadeiro")
+ }
+ if(n1==n2) {
+     console.log("Errado")
+ } else {
+     console.log("certo")
+ }
+
+   
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
